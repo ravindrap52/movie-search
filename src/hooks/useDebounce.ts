28d8514b3df31dc231e1763ab4450bf1bf50efa0
ravindra pallikonda <ravindra.pallikonda@ah-mt.com>
2024-07-21 20:01:15 +0200

    docs(movie-search): added function signature

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 
+/** 
+ * updating the debounced value
+ * 
+ * @param {string} value - The value to be debounced.
+ * @param {number} delay - The debounce delay in milliseconds.
+ * @returns {string} - The debounced version of the input value.
+ */
 export const useDebounce = (value: string, delay: number): string => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
