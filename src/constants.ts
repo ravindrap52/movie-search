b219ba55985cc8358333d890d63de52f27f03f0b
ravindra pallikonda <ravindra.pallikonda@ah-mt.com>
2024-07-21 19:28:04 +0200

    fix(movie-search): fixed url

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://www.omdbapi.com"
+export const API_URL = "https://www.omdbapi.com"
 export const API_KEY = "c05820ad";
 export const PLACEHOLDER_IMAGE = "https://via.placeholder.com/300x200"
